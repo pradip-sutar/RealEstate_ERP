8b6fddb44ef1b0975c281aae15e3634dc27f3365
pradip-sutar <pradip.sutar2018@gmail.com>
2024-07-11 22:55:27 +0530

    deparetment handler

--- Employee_Management/urls.py
+++ Employee_Management/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('employee_management_handler/', employee_management_handler, name='employee_management_handler'),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('employee_salary_handler/', employee_salary_handler, name='employee_salary_handler'),
     path('employee_document_handler/', employee_document_handler, name='employee_document_handler'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
