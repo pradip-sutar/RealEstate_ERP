47f0d5d34b32d48f29f98420a45fd56e00215770
pradip-sutar <pradip.sutar2018@gmail.com>
2024-09-05 15:32:33 +0530

    update admin.py in roles and rights

--- Roles_Rights/models.py
+++ Roles_Rights/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Roles(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self):
-        return self.name
+        return f"{self.name}"
     
 class Rights(models.Model):
     roles = models.ForeignKey(Roles, on_delete=models.CASCADE)
@@ -13,4 +13,4 @@ class Rights(models.Model):
     delete = models.BooleanField(default=False)
     # ischecked = models.BooleanField(default=False)
     def __str__(self):
-        return self.name
+        return f"{self.roles}"
